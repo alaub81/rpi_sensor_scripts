325fc082f71daaf4f525fb15996fab25642c0da4
root <root@laub-raspi4.laub.loc>
2021-02-09 18:14:51 +0100

    Initial Release

---
+++ tsl2561.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import board
+import adafruit_tsl2561
+
+i2c = board.I2C()
+sensor = adafruit_tsl2561.TSL2561(i2c)
+
+# Set high gain mode.
+# 0 is low gain 1 is high gain
+sensor.gain = 1
+# Set integration time.
+# A value 0 is 13.7ms, 1 is 101ms, 2 is 402ms, and 3 is manual mode.
+sensor.integration_time = 2
+
+print('Lux: {}'.format(sensor.lux))
+print('Broadband: {}'.format(sensor.broadband))
+print('Infrared: {}'.format(sensor.infrared))
+print('Luminosity: {}'.format(sensor.luminosity))
