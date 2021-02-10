e16b66199e70a35cf018469ca67855368c112469
Andreas Laub <andreas@laub-home.de>
2021-02-10 14:51:38 +0100

    Update tsl2561.py

--- tsl2561.py
+++ tsl2561.py
@@ -7,10 +7,10 @@ sensor = adafruit_tsl2561.TSL2561(i2c)
 
 # Set high gain mode.
 # 0 is low gain 1 is high gain
-sensor.gain = 1
+sensor.gain = 0
 # Set integration time.
 # A value 0 is 13.7ms, 1 is 101ms, 2 is 402ms, and 3 is manual mode.
-sensor.integration_time = 2
+sensor.integration_time = 0
 
 print('Lux: {}'.format(sensor.lux))
 print('Broadband: {}'.format(sensor.broadband))
